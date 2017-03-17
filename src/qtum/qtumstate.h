ffd3c6f8abdb6c001ce172d83a56c600b4859976
Alex Dulub <alex.dulub@designsters.com>
2017-03-17 13:17:49 +0300

    Added files qtumstate.

---
+++ src/qtum/qtumstate.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <libethereum/State.h>
+#include <libevm/ExtVMFace.h>
+
+using OnOpFunc = std::function<void(uint64_t, uint64_t, dev::eth::Instruction, dev::bigint, dev::bigint, dev::bigint, dev::eth::VM*, dev::eth::ExtVMFace const*)>;
+
+class QtumState : public dev::eth::State {
+    
+public:
+
+    QtumState() : State(dev::Invalid256, dev::OverlayDB(), dev::eth::BaseState::PreExisting) {}
+
+    QtumState(dev::u256 const& _accountStartNonce, dev::OverlayDB const& _db, dev::eth::BaseState _bs = dev::eth::BaseState::PreExisting) :
+        dev::eth::State(_accountStartNonce, _db, _bs) {}
+
+    std::pair<dev::eth::ExecutionResult, dev::eth::TransactionReceipt> execute(dev::eth::EnvInfo const& _envInfo, dev::eth::SealEngineFace const& _sealEngine, dev::eth::Transaction const& _t, dev::eth::Permanence _p, OnOpFunc const& _onOp){
+        std::pair<dev::eth::ExecutionResult, dev::eth::TransactionReceipt> res = State::execute(_envInfo, _sealEngine, _t, _p, _onOp);
+        dev::eth::Account const* ac = account(_t.sender());
+        // ac->kill();
+        // commit(CommitBehaviour::RemoveEmptyAccounts);
+        db().commit();
+        return res;
+    }
+
+private:
+
+};
