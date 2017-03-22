52e3f35c0f68e42518de880a14a0469dfb537487
Time Markov <time_markov@bitse.com>
2017-03-22 02:23:35 -0700

    Update the test for amount compression to check up to billion coins.

--- src/test/compress_tests.cpp
+++ src/test/compress_tests.cpp
@@ -19,8 +19,8 @@
 // amounts 1 .. 10000
 #define NUM_MULTIPLES_1BTC 10000
 
-// amounts 50 .. 21000000
-#define NUM_MULTIPLES_50BTC 420000
+// amounts 50 .. 100000000
+#define NUM_MULTIPLES_50BTC 2000000
 
 BOOST_FIXTURE_TEST_SUITE(compress_tests, BasicTestingSetup)
 
@@ -39,12 +39,13 @@ bool static TestPair(uint64_t dec, uint64_t enc) {
 
 BOOST_AUTO_TEST_CASE(compress_amounts)
 {
-    BOOST_CHECK(TestPair(            0,       0x0));
-    BOOST_CHECK(TestPair(            1,       0x1));
-    BOOST_CHECK(TestPair(         CENT,       0x7));
-    BOOST_CHECK(TestPair(         COIN,       0x9));
-    BOOST_CHECK(TestPair(      50*COIN,      0x32));
-    BOOST_CHECK(TestPair(21000000*COIN, 0x1406f40));
+    BOOST_CHECK(TestPair(              0,        0x0));
+    BOOST_CHECK(TestPair(              1,        0x1));
+    BOOST_CHECK(TestPair(           CENT,        0x7));
+    BOOST_CHECK(TestPair(           COIN,        0x9));
+    BOOST_CHECK(TestPair(        50*COIN,       0x32));
+    BOOST_CHECK(TestPair( 100000000*COIN,  0x5F5E100));
+    BOOST_CHECK(TestPair(1000000000*COIN, 0x3B9ACA00));
 
     for (uint64_t i = 1; i <= NUM_MULTIPLES_UNIT; i++)
         BOOST_CHECK(TestEncode(i));
