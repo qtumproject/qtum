d565b4e04df8ce2eda8e3d4b7c0fe7e0c644e799
Time Markov <time_markov@bitse.com>
2017-03-29 02:24:23 -0700

    Update the max amount of coins to 108.8M

--- src/amount.h
+++ src/amount.h
@@ -20,7 +20,7 @@ static const CAmount CENT = 1000000;
 extern const std::string CURRENCY_UNIT;
 
 /** No amount larger than this (in satoshi) is valid.*/
-static const CAmount MAX_MONEY = std::numeric_limits<int64_t>::max();
+static const CAmount MAX_MONEY = 107822406 * COIN + 25 * CENT;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /**
