8374aa70e4068748cc640a75b235895ab08da4a3
Alex Dulub <alex.dulub@designsters.com>
2017-04-07 18:05:15 +0300

    virtual destructor fix; shutdown thread renamed

--- src/init.cpp
+++ src/init.cpp
@@ -192,7 +192,7 @@ void Shutdown()
     /// for example if the data directory was found to be locked.
     /// Be sure that anything that writes files or flushes caches only does this if the respective
     /// module was initialized.
-    RenameThread("bitcoin-shutoff");
+    RenameThread("qtum-shutoff");
     mempool.AddTransactionsUpdated(1);
 
     StopHTTPRPC();
