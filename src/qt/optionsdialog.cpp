74384fe0923c3fda693148593faae368a3196297
Toni Markov <toni.qtum@gmail.com>
2017-04-27 10:26:37 +0200

    Connect the mapper for reserve balance in the options dialog.

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -179,6 +179,7 @@ void OptionsDialog::setMapper()
     mapper->addMapping(ui->bitcoinAtStartup, OptionsModel::StartAtStartup);
     mapper->addMapping(ui->threadsScriptVerif, OptionsModel::ThreadsScriptVerif);
     mapper->addMapping(ui->databaseCache, OptionsModel::DatabaseCache);
+    mapper->addMapping(ui->reserveBalance, OptionsModel::ReserveBalance);
 
     /* Wallet */
     mapper->addMapping(ui->spendZeroConfChange, OptionsModel::SpendZeroConfChange);
