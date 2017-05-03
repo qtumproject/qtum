75842c3b7f1d665f881336923d057b5e837e55a5
ubuntu <david.jaenson@gmail.com>
2017-05-03 16:47:02 +0200

    Added fixes to tests that include the hashUTXORoot and the expected output from getaccountinfo

--- qa/rpc-tests/rest.py
+++ qa/rpc-tests/rest.py
@@ -220,9 +220,9 @@ class RESTTest (BitcoinTestFramework):
         # compare with block header
         response_header = http_get_call(url.hostname, url.port, '/rest/headers/1/'+bb_hash+self.FORMAT_SEPARATOR+"bin", True)
         assert_equal(response_header.status, 200)
-        assert_equal(int(response_header.getheader('content-length')), 154)
+        assert_equal(int(response_header.getheader('content-length')), 186)
         response_header_str = response_header.read()
-        assert_equal(response_str[0:154], response_header_str)
+        assert_equal(response_str[0:186], response_header_str)
 
         # check block hex format
         response_hex = http_get_call(url.hostname, url.port, '/rest/block/'+bb_hash+self.FORMAT_SEPARATOR+"hex", True)
