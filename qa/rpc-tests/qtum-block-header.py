7bdee206738827472d9d259eed77c89ba1856cd3
ubuntu <david.jaenson@gmail.com>
2017-05-09 04:57:05 +0200

    Added tests for the state and utxo roots, fixed a typo in CBlockHeader

---
+++ qa/rpc-tests/qtum-block-header.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# Copyright (c) 2015-2016 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from test_framework.test_framework import ComparisonTestFramework
+from test_framework.util import *
+from test_framework.comptool import TestManager, TestInstance, RejectResult
+from test_framework.blocktools import *
+from test_framework.mininode import *
+from test_framework.address import *
+from test_framework.qtum import *
+import time
+from test_framework.key import CECKey
+from test_framework.script import *
+import struct
+import io
+
+
+def find_unspent(node, amount):
+    for unspent in node.listunspent():
+        print(unspent)
+        if unspent['amount'] == amount and unspent['spendable']:
+            return CTxIn(COutPoint(int(unspent['txid'], 16), unspent['vout']), nSequence=0)
+    assert(False)
+
+
+class QtumBlockHeaderTest(ComparisonTestFramework):
+    def __init__(self):
+        super().__init__()
+        self.num_nodes = 1
+        self.tip = None
+
+    def add_options(self, parser):
+        super().add_options(parser)
+        parser.add_option("--runbarelyexpensive", dest="runbarelyexpensive", default=True)
+
+    def run_test(self):
+        self.test = TestManager(self, self.options.tmpdir)
+        self.test.add_all_connections(self.nodes)
+        NetworkThread().start() # Start up network handling in another thread
+        self.test.run()
+
+    def get_tests(self):
+        # returns a test case that asserts that the current tip was accepted
+        def accepted():
+            return TestInstance([[self.tip, True]])
+
+        # returns a test case that asserts that the current tip was rejected
+        def rejected(reject = None):
+            if reject is None:
+                return TestInstance([[self.tip, False]])
+            else:
+                return TestInstance([[self.tip, reject]])
+
+        node = self.nodes[0]
+        mocktime = 1490247077
+        node.setmocktime(mocktime)
+
+        node.generate(50)
+        spendable_addresses = []
+        # store some addresses to use later
+        for unspent in node.listunspent():
+            spendable_addresses.append(unspent['address'])
+
+        # first make sure that what is a valid block is accepted
+        coinbase = create_coinbase(node.getblockcount()+1)
+        coinbase.rehash()
+        self.tip = create_block(int(node.getbestblockhash(), 16), coinbase, int(mocktime+100))
+        self.tip.hashMerkleRoot = self.tip.calc_merkle_root()
+        self.tip.solve()
+        yield accepted()
+
+        # A block that has an OP_CREATE tx, butwith an incorrect state root
+        """
+            pragma solidity ^0.4.11;
+            contract Test {
+                function() payable {}
+            }
+        """
+        tx_hex = node.createcontract("60606040523415600b57fe5b5b60398060196000396000f30060606040525b600b5b5b565b0000a165627a7a72305820693c4900c412f72a51f8c01a36d38d9038d822d953faf5a5b28e40ec6e1a25020029", 1000000, 0.00000001, spendable_addresses.pop(-1), False)['raw transaction']
+        f = io.BytesIO(hex_str_to_bytes(tx_hex))
+        tx = CTransaction()
+        tx.deserialize(f)
+
+        coinbase = create_coinbase(node.getblockcount()+1)
+        coinbase.rehash()
+        self.tip = create_block(int(node.getbestblockhash(), 16), coinbase, int(mocktime+200))
+        self.tip.vtx.append(tx)
+        self.tip.hashMerkleRoot = self.tip.calc_merkle_root()
+        self.tip.solve()
+        yield rejected()
+
+
+        # Create a contract for use later.
+        """
+            pragma solidity ^0.4.11;
+            contract Test {
+                function() payable {}
+            }
+        """
+        contract_address = node.createcontract("60606040523415600b57fe5b5b60398060196000396000f30060606040525b600b5b5b565b0000a165627a7a72305820693c4900c412f72a51f8c01a36d38d9038d822d953faf5a5b28e40ec6e1a25020029")['address']
+        node.generate(1)
+
+        realHashUTXORoot = int(node.getblock(node.getbestblockhash())['hashUTXORoot'], 16)
+        realHashStateRoot = int(node.getblock(node.getbestblockhash())['hashStateRoot'], 16)
+
+        # A block with both an invalid hashStateRoot and hashUTXORoot
+        coinbase = create_coinbase(node.getblockcount()+1)
+        coinbase.rehash()
+        self.tip = create_block(int(node.getbestblockhash(), 16), coinbase, int(mocktime+300))
+        self.tip.hashUTXORoot = 0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        self.tip.hashStateRoot = 0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        self.tip.hashMerkleRoot = self.tip.calc_merkle_root()
+        self.tip.solve()
+        yield rejected()
+
+
+        # A block with a tx, but without updated state hashes
+        tx_hex = node.sendtocontract(contract_address, "00", 1, 100000, 0.00000001, spendable_addresses.pop(-1), False)['raw transaction']
+        f = io.BytesIO(hex_str_to_bytes(tx_hex))
+        tx = CTransaction()
+        tx.deserialize(f)
+
+        coinbase = create_coinbase(node.getblockcount()+1)
+        coinbase.rehash()
+        self.tip = create_block(int(node.getbestblockhash(), 16), coinbase, int(mocktime+400))
+        self.tip.realHashUTXORoot = realHashUTXORoot
+        self.tip.realHashStateRoot = realHashStateRoot
+        self.tip.vtx.append(tx)
+        self.tip.hashMerkleRoot = self.tip.calc_merkle_root()
+        self.tip.solve()
+        yield rejected()
+
+        # A block with an invalid hashUTXORoot
+        coinbase = create_coinbase(node.getblockcount()+1)
+        coinbase.rehash()
+        self.tip = create_block(int(node.getbestblockhash(), 16), coinbase, int(mocktime+500))
+        self.tip.hashStateRoot = realHashStateRoot
+        self.tip.hashUTXORoot = 0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        self.tip.hashMerkleRoot = self.tip.calc_merkle_root()
+        self.tip.solve()
+        yield rejected()
+
+        # A block with an invalid hashStateRoot
+        coinbase = create_coinbase(node.getblockcount()+1)
+        coinbase.rehash()
+        self.tip = create_block(int(node.getbestblockhash(), 16), coinbase, int(mocktime+600))
+        self.tip.hashUTXORoot = realHashUTXORoot
+        self.tip.hashStateRoot = 0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        self.tip.hashMerkleRoot = self.tip.calc_merkle_root()
+        self.tip.solve()
+        yield rejected()
+
+        # Verify that blocks with a correct hashStateRoot and hashUTXORoot are accepted.
+        coinbase = create_coinbase(node.getblockcount()+1)
+        coinbase.rehash()
+        self.tip = create_block(int(node.getbestblockhash(), 16), coinbase, int(mocktime+700))
+        self.tip.hashUTXORoot = realHashUTXORoot
+        self.tip.hashStateRoot = realHashStateRoot
+        self.tip.hashMerkleRoot = self.tip.calc_merkle_root()
+        self.tip.solve()
+        yield accepted()
+
+
+if __name__ == '__main__':
+    QtumBlockHeaderTest().main()
