5d8f83ae1f493ee6cd0c523f78cd1f9173e9366d
Time Markov <time_markov@bitse.com>
2017-08-10 08:17:34 -0700

    Add wallet frame border

--- src/qt/walletframe.cpp
+++ src/qt/walletframe.cpp
@@ -21,6 +21,7 @@ WalletFrame::WalletFrame(const PlatformStyle *_platformStyle, BitcoinGUI *_gui) 
     QHBoxLayout *walletFrameLayout = new QHBoxLayout(this);
     setContentsMargins(0,0,0,0);
     walletStack = new QStackedWidget(this);
+    walletStack->setStyleSheet(".QStackedWidget {border: 1px solid #c4c1bd;}");
     walletFrameLayout->setContentsMargins(0,0,0,0);
     walletFrameLayout->addWidget(walletStack);
 
