6c6e87f9751f81d5501987c59dc525e8f88615a5
Alex Dulub <alex.dulub@designsters.com>
2017-09-06 11:53:49 +0300

    RPC call to createcontract segfaults when passing large contracts.

--- src/util.cpp
+++ src/util.cpp
@@ -867,3 +867,18 @@ std::string CopyrightHolders(const std::string& strPrefix)
     }
     return strCopyrightHolders;
 }
+
+bool CheckHex(std::string& str) {
+    if(str.size() > 10000){
+        std::string tempStr = str;
+        while(!tempStr.empty()){
+            std::string part(tempStr.begin(), tempStr.size() >= 1000 ? (tempStr.begin() + 1000) : tempStr.end());
+            if(part.size() % 2 != 0 || !std::regex_match(part, hexData)){
+                return false;
+            }
+            tempStr.erase(tempStr.begin(), tempStr.size() >= 1000 ? (tempStr.begin() + 1000) : tempStr.end());
+        }
+        return true;
+    }
+    return std::regex_match(str, hexData);
+}
