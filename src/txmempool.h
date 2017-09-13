d443725ef4673075db8c0fd76b72994ff955e204
Neil <neil@qtum.org>
2017-09-13 17:42:15 +0800

    Fix miner tests

--- src/txmempool.h
+++ src/txmempool.h
@@ -43,7 +43,7 @@ inline bool AllowFree(double dPriority)
 }
 
 /** Fake height value used in Coin to signify they are only in the memory pool (since 0.8) */
-static const uint32_t MEMPOOL_HEIGHT = 0x7FFFFFFF;
+static const uint32_t MEMPOOL_HEIGHT = 0x0FFFFFFF;
 
 struct LockPoints
 {
