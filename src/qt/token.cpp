37383aeedf756410c0afb513241621cd48862feb
Time Markov <time.markov@qtum.com>
2017-09-25 07:21:33 -0700

    Add token ABI

---
+++ src/qt/token.cpp
@@ -0,0 +1,510 @@
+#include "token.h"
+#include "execrpccommand.h"
+#include "contractabi.h"
+#include "validation.h"
+#include "utilmoneystr.h"
+
+namespace Token_NS
+{
+static const std::string TOKEN_ABI = "[{\"constant\":true,\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_spender\",\"type\":\"std::string\"},{\"name\":\"_value\",\"type\":\"std::string\"}],\"name\":\"approve\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"name\":\"\",\"type\":\"std::string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_from\",\"type\":\"std::string\"},{\"name\":\"_to\",\"type\":\"std::string\"},{\"name\":\"_value\",\"type\":\"std::string\"}],\"name\":\"transferFrom\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"decimals\",\"outputs\":[{\"name\":\"\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_value\",\"type\":\"std::string\"}],\"name\":\"burn\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"\",\"type\":\"std::string\"}],\"name\":\"balanceOf\",\"outputs\":[{\"name\":\"\",\"type\":\"std::string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_from\",\"type\":\"std::string\"},{\"name\":\"_value\",\"type\":\"std::string\"}],\"name\":\"burnFrom\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"symbol\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"std::string\"},{\"name\":\"_value\",\"type\":\"std::string\"}],\"name\":\"transfer\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_spender\",\"type\":\"std::string\"},{\"name\":\"_value\",\"type\":\"std::string\"},{\"name\":\"_extraData\",\"type\":\"std::string\"}],\"name\":\"approveAndCall\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"\",\"type\":\"std::string\"},{\"name\":\"\",\"type\":\"std::string\"}],\"name\":\"allowance\",\"outputs\":[{\"name\":\"\",\"type\":\"std::string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"name\":\"initialSupply\",\"type\":\"std::string\"},{\"name\":\"tokenName\",\"type\":\"string\"},{\"name\":\"decimalUnits\",\"type\":\"uint8\"},{\"name\":\"tokenSymbol\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"from\",\"type\":\"std::string\"},{\"indexed\":true,\"name\":\"to\",\"type\":\"std::string\"},{\"indexed\":false,\"name\":\"value\",\"type\":\"std::string\"}],\"name\":\"Transfer\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"from\",\"type\":\"std::string\"},{\"indexed\":false,\"name\":\"value\",\"type\":\"std::string\"}],\"name\":\"Burn\",\"type\":\"event\"}]";
+
+static const QString PRC_CALL = "callcontract";
+static const QString PRC_SENDTO = "sendtocontract";
+static const QString PARAM_ADDRESS = "address";
+static const QString PARAM_DATAHEX = "datahex";
+static const QString PARAM_AMOUNT = "amount";
+static const QString PARAM_GASLIMIT = "gaslimit";
+static const QString PARAM_GASPRICE = "gasprice";
+static const QString PARAM_SENDER = "sender";
+}
+using namespace Token_NS;
+
+struct TokenData
+{
+    QMap<QString, QString> lstParams;
+    std::string address;
+    ExecRPCCommand* call;
+    ExecRPCCommand* send;
+    ContractABI* ABI;
+    int funcName;
+    int funcApprove;
+    int funcTotalSupply;
+    int funcTransferFrom;
+    int funcDecimals;
+    int funcBurn;
+    int funcBalanceOf;
+    int funcBurnFrom;
+    int funcSymbol;
+    int funcTransfer;
+    int funcApproveAndCall;
+    int funcAllowance;
+    int evtTransfer;
+    int evtBurn;
+
+    TokenData():
+        call(0),
+        send(0),
+        ABI(0),
+        funcName(-1),
+        funcApprove(-1),
+        funcTotalSupply(-1),
+        funcTransferFrom(-1),
+        funcDecimals(-1),
+        funcBurn(-1),
+        funcBalanceOf(-1),
+        funcBurnFrom(-1),
+        funcSymbol(-1),
+        funcTransfer(-1),
+        funcApproveAndCall(-1),
+        funcAllowance(-1),
+        evtTransfer(-1),
+        evtBurn(-1)
+    {}
+};
+
+Token::Token():
+    d(0)
+{
+    d = new TokenData();
+    clear();
+
+    // Create new call command line interface
+    QStringList lstMandatory;
+    lstMandatory.append(PARAM_ADDRESS);
+    lstMandatory.append(PARAM_DATAHEX);
+    QStringList lstOptional;
+    lstOptional.append(PARAM_SENDER);
+    d->call = new ExecRPCCommand(PRC_CALL, lstMandatory, lstOptional, QMap<QString, QString>());
+
+    // Create new send command line interface
+    lstMandatory.clear();
+    lstMandatory.append(PARAM_ADDRESS);
+    lstMandatory.append(PARAM_DATAHEX);
+    lstOptional.clear();
+    lstOptional.append(PARAM_AMOUNT);
+    lstOptional.append(PARAM_GASLIMIT);
+    lstOptional.append(PARAM_GASPRICE);
+    lstOptional.append(PARAM_SENDER);
+    d->send = new ExecRPCCommand(PRC_SENDTO, lstMandatory, lstOptional, QMap<QString, QString>());
+
+    // Compute functions indexes
+    d->ABI = new ContractABI();
+    if(d->ABI->loads(TOKEN_ABI))
+    {
+        for(size_t i = 0; i < d->ABI->functions.size(); i++)
+        {
+            FunctionABI func = d->ABI->functions[i];
+            if(func.name == "name")
+            {
+                d->funcName = i;
+            }
+            else if(func.name == "approve")
+            {
+                d->funcApprove = i;
+            }
+            else if(func.name == "totalSupply")
+            {
+                d->funcTotalSupply = i;
+            }
+            else if(func.name == "transferFrom")
+            {
+                d->funcTransferFrom = i;
+            }
+            else if(func.name == "decimals")
+            {
+                d->funcDecimals = i;
+            }
+            else if(func.name == "burn")
+            {
+                d->funcBurn = i;
+            }
+            else if(func.name == "balanceOf")
+            {
+                d->funcBalanceOf = i;
+            }
+            else if(func.name == "burnFrom")
+            {
+                d->funcBurnFrom = i;
+            }
+            else if(func.name == "symbol")
+            {
+                d->funcSymbol = i;
+            }
+            else if(func.name == "transfer")
+            {
+                d->funcTransfer = i;
+            }
+            else if(func.name == "approveAndCall")
+            {
+                d->funcApproveAndCall = i;
+            }
+            else if(func.name == "allowance")
+            {
+                d->funcAllowance = i;
+            }
+            else if(func.name == "Transfer")
+            {
+                d->evtTransfer = i;
+            }
+            else if(func.name == "Burn")
+            {
+                d->evtBurn = i;
+            }
+        }
+    }
+}
+
+Token::~Token()
+{
+    if(d->call)
+        delete d->call;
+    d->call = 0;
+
+    if(d->send)
+        delete d->send;
+    d->send = 0;
+
+    if(d->ABI)
+        delete d->ABI;
+    d->ABI = 0;
+
+    if(d)
+        delete d;
+    d = 0;
+}
+
+void Token::setAddress(const std::string &address)
+{
+    ExecRPCCommand::appendParam(d->lstParams, PARAM_ADDRESS, QString::fromStdString(address));
+}
+
+void Token::setDataHex(const std::string &datahex)
+{
+    ExecRPCCommand::appendParam(d->lstParams, PARAM_DATAHEX, QString::fromStdString(datahex));
+}
+
+void Token::setAmount(const std::string &amount)
+{
+    ExecRPCCommand::appendParam(d->lstParams, PARAM_AMOUNT, QString::fromStdString(amount));
+}
+
+void Token::setGasLimit(const std::string &gaslimit)
+{
+    ExecRPCCommand::appendParam(d->lstParams, PARAM_GASLIMIT, QString::fromStdString(gaslimit));
+}
+
+void Token::setGasPrice(const std::string &gasPrice)
+{
+    ExecRPCCommand::appendParam(d->lstParams, PARAM_GASPRICE, QString::fromStdString(gasPrice));
+}
+
+void Token::setSender(const std::string &sender)
+{
+    ExecRPCCommand::appendParam(d->lstParams, PARAM_SENDER, QString::fromStdString(sender));
+}
+
+void Token::clear()
+{
+    d->lstParams.clear();
+
+    setAmount("0");
+    setGasPrice(FormatMoney(DEFAULT_GAS_PRICE));
+    setGasLimit(std::to_string(DEFAULT_GAS_LIMIT_OP_SEND));
+}
+
+bool Token::name(std::string &result, bool sendTo)
+{
+    std::vector<std::string> input;
+    std::vector<std::string> output;
+    if(!exec(input, d->funcName, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            result = output[0];
+    }
+
+    return true;
+}
+
+bool Token::approve(const std::string &_spender, const std::string &_value, bool &success, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(_spender);
+    input.push_back(_value);
+    std::vector<std::string> output;
+
+    if(!exec(input, d->funcApprove, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            success = output[0] == "true";
+    }
+
+    return true;
+}
+
+bool Token::totalSupply(std::string &result, bool sendTo)
+{
+    std::vector<std::string> input;
+    std::vector<std::string> output;
+    if(!exec(input, d->funcTotalSupply, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            result = output[0];
+    }
+
+    return true;
+}
+
+bool Token::transferFrom(const std::string &_from, const std::string &_to, const std::string &_value, bool &success, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(_from);
+    input.push_back(_to);
+    input.push_back(_value);
+    std::vector<std::string> output;
+
+    if(!exec(input, d->funcTransferFrom, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            success = output[0] == "true";
+    }
+
+    return true;
+}
+
+bool Token::decimals(std::string &result, bool sendTo)
+{
+    std::vector<std::string> input;
+    std::vector<std::string> output;
+    if(!exec(input, d->funcDecimals, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            result = output[0];
+    }
+
+    return true;
+}
+
+bool Token::burn(const std::string &_value, bool &success, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(_value);
+    std::vector<std::string> output;
+
+    if(!exec(input, d->funcBurn, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            success = output[0] == "true";
+    }
+
+    return true;
+}
+
+bool Token::balanceOf(const std::string &spender, std::string &result, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(spender);
+    std::vector<std::string> output;
+
+    if(!exec(input, d->funcBalanceOf, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            result = output[0];
+    }
+
+    return true;
+}
+
+bool Token::burnFrom(const std::string &_from, const std::string &_value, bool &success, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(_from);
+    input.push_back(_value);
+    std::vector<std::string> output;
+
+    if(!exec(input, d->funcBurnFrom, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            success = output[0] == "true";
+    }
+
+    return true;
+}
+
+bool Token::symbol(std::string &result, bool sendTo)
+{
+    std::vector<std::string> input;
+    std::vector<std::string> output;
+    if(!exec(input, d->funcSymbol, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            result = output[0];
+    }
+
+    return true;
+}
+
+bool Token::transfer(const std::string &_to, const std::string &_value, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(_to);
+    input.push_back(_value);
+    std::vector<std::string> output;
+
+    return exec(input, d->funcTransfer, output, sendTo);
+}
+
+bool Token::approveAndCall(const std::string &_spender, const std::string &_value, const std::string &_extraData, bool &success, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(_spender);
+    input.push_back(_value);
+    input.push_back(_extraData);
+    std::vector<std::string> output;
+
+    if(!exec(input, d->funcApproveAndCall, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            success = output[0] == "true";
+    }
+
+    return true;
+}
+
+bool Token::allowance(const std::string &_from, const std::string &_to, std::string &result, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(_from);
+    input.push_back(_to);
+    std::vector<std::string> output;
+
+    if(!exec(input, d->funcAllowance, output, sendTo))
+        return false;
+
+    if(!sendTo)
+    {
+        if(output.size() == 0)
+            return false;
+        else
+            result = output[0];
+    }
+
+    return true;
+}
+
+bool Token::Transfer(const std::string &from, const std::string &to, const std::string &value, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(from);
+    input.push_back(to);
+    input.push_back(value);
+    std::vector<std::string> output;
+
+    return exec(input, d->evtTransfer, output, sendTo);
+}
+
+bool Token::Burn(const std::string &from, const std::string &value, bool sendTo)
+{
+    std::vector<std::string> input;
+    input.push_back(from);
+    input.push_back(value);
+    std::vector<std::string> output;
+
+    return exec(input, d->evtBurn, output, sendTo);
+}
+
+bool Token::exec(const std::vector<std::string> &input, int func, std::vector<std::string> &output, bool sendTo)
+{
+    // Convert the input data into hex encoded binary data
+    if(func == -1)
+        return false;
+    std::string strData;
+    FunctionABI function = d->ABI->functions[func];
+    std::vector<std::vector<std::string>> values;
+    for(size_t i = 0; i < input.size(); i++)
+    {
+        std::vector<std::string> param;
+        param.push_back(input[i]);
+        values.push_back(param);
+    }
+    std::vector<ParameterABI::ErrorType> errors;
+    if(!function.abiIn(values, strData, errors))
+        return false;
+    setDataHex(strData);
+
+    // Execute the command and get the result
+    ExecRPCCommand* cmd = sendTo ? d->send : d->call;
+    QVariant result;
+    QString resultJson;
+    QString errorMessage;
+    if(!cmd->exec(d->lstParams, result, resultJson, errorMessage))
+        return false;
+
+    // Get the result from calling function
+    if(!sendTo)
+    {
+        QVariantMap variantMap = result.toMap();
+        QVariantMap executionResultMap = variantMap.value("executionResult").toMap();
+        std::string rawData = executionResultMap.value("output").toString().toStdString();
+        std::vector<std::vector<std::string>> values;
+        std::vector<ParameterABI::ErrorType> errors;
+        if(!function.abiOut(rawData, values, errors))
+            return false;
+        for(size_t i = 0; i < values.size(); i++)
+        {
+            std::vector<std::string> param = values[i];
+            output.push_back(param.size() ? param[0] : "");
+        }
+    }
+
+    return true;
+}
