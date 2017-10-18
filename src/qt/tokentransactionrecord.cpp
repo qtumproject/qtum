bb2b43d3f3d05a8109d2beeef6827331a41e05b4
Time Markov <time.markov@qtum.com>
2017-10-18 12:15:41 -0700

    Update token transaction decomposition

--- src/qt/tokentransactionrecord.cpp
+++ src/qt/tokentransactionrecord.cpp
@@ -15,7 +15,58 @@
  */
 QList<TokenTransactionRecord> TokenTransactionRecord::decomposeTransaction(const CWallet *wallet, const CTokenTx &wtx)
 {
-    return QList<TokenTransactionRecord>();
+    // Initialize variables
+    QList<TokenTransactionRecord> parts;
+    uint256 credit;
+    uint256 debit;
+    std::string tokenSymbol;
+    uint8_t decimals = 18;
+    if(wallet && wallet->GetTokenTxDetails(wtx, credit, debit, tokenSymbol, decimals))
+    {
+        // Get token transaction data
+        TokenTransactionRecord rec;
+        rec.time = wtx.nTime;
+        rec.credit = dev::u2s(uintTou256(credit));
+        rec.debit = -dev::u2s(uintTou256(debit));
+        rec.hash = wtx.transactionHash;
+        rec.tokenSymbol = tokenSymbol;
+        rec.decimals = decimals;
+        dev::s256 net = rec.credit + rec.debit;
+
+        // Determine type
+        if(wtx.strSenderAddress == wtx.strReceiverAddress)
+        {
+            rec.type = SendToSelf;
+        }
+        else
+        {
+            if(net > 0)
+                rec.type = RecvWithAddress;
+
+            if(net < 0)
+                rec.type = SendToAddress;
+        }
+
+        // Set address
+        switch (rec.type) {
+        case SendToAddress:
+        case SendToOther:
+        case SendToSelf:
+            rec.address = wtx.strSenderAddress;
+            break;
+        case RecvWithAddress:
+        case RecvFromOther:
+            rec.address = wtx.strReceiverAddress;
+        default:
+            break;
+        }
+
+        // Append record
+        if(rec.type != Other)
+            parts.append(rec);
+    }
+
+    return parts;
 }
 
 void TokenTransactionRecord::updateStatus(const CTokenTx &wtx)
