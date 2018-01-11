068e6168812333a9c49f4a515b86f1b555edb947
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-01-11 20:04:59 +0700

    rpc: Fix SoftForkMajorityDesc and SoftForkDesc signatures
    (cherry picked from commit 54dc13b6a2ea2823d71968049c9ad80648c3d962)

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1973,7 +1973,7 @@ static UniValue verifychain(const JSONRPCRequest& request)
 }
 
 /** Implementation of IsSuperMajority with better feedback */
-static UniValue SoftForkMajorityDesc(int version, CBlockIndex* pindex, const Consensus::Params& consensusParams)
+static UniValue SoftForkMajorityDesc(int version, const CBlockIndex* pindex, const Consensus::Params& consensusParams)
 {
     UniValue rv(UniValue::VOBJ);
     bool activated = false;
@@ -1993,7 +1993,7 @@ static UniValue SoftForkMajorityDesc(int version, CBlockIndex* pindex, const Con
     return rv;
 }
 
-static UniValue SoftForkDesc(const std::string &name, int version, CBlockIndex* pindex, const Consensus::Params& consensusParams)
+static UniValue SoftForkDesc(const std::string &name, int version, const CBlockIndex* pindex, const Consensus::Params& consensusParams)
 {
     UniValue rv(UniValue::VOBJ);
     rv.pushKV("id", name);
