72c943914d93c7603fb68253ab893fe28807c544
Alex Dulub <alex.dulub@designsters.com>
2018-02-09 19:01:42 +0300

    added qtum-detached-sigs repo

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -219,6 +219,7 @@ if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
     git clone https://github.com/qtumproject/gitian.sigs.git
+    git clone https://github.com/qtumproject/qtum-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
