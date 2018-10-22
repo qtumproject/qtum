5234e0fe2f8598d7f46caef903c3a1043e37f429
Time Markov <time.markov@qtum.com>
2018-10-22 13:07:44 +0200

    Update header filter for zero size

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -172,7 +172,7 @@ public:
 
     bool addHeaders(const CBlockIndex *pindexFirst, const CBlockIndex *pindexLast)
     {
-        if(pindexFirst && pindexLast)
+        if(pindexFirst && pindexLast && maxSize && maxAvg)
         {
             // Get the begin block index
             int nBegin = pindexFirst->nHeight;
@@ -196,7 +196,7 @@ public:
         // No headers
         size_t size = points.size();
         if(size == 0)
-            return true;
+            return ret;
 
         // Compute the number of the received headers
         size_t nHeaders = 0;
