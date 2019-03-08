82400c59de64faa9e6a4f8f2a257b17771190fef
Time Markov <time.markov@qtum.com>
2019-03-08 15:23:43 +0100

    Add new target timespan parameter that produce target multiplier of 20000

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -74,6 +74,7 @@ struct Params {
     bool fPoSNoRetargeting;
     int64_t nPowTargetSpacing;
     int64_t nPowTargetTimespan;
+    int64_t nPowTargetTimespanV2;
     int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
     uint256 nMinimumChainWork;
     uint256 defaultAssumeValid;
