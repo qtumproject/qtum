10b9623544bb388b78e7daed734d90b1b5f47312
Time Markov <time.markov@qtum.com>
2019-06-13 10:59:33 +0200

    Skip coinstake tx from block stats

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -2688,7 +2688,7 @@ static UniValue getblockstats(const JSONRPCRequest& request)
             }
         }
 
-        if (tx->IsCoinBase()) {
+        if (tx->IsCoinBase() || tx->IsCoinStake()) {
             continue;
         }
 
