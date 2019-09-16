091a94f83ee94adf5a5d907b88c28237082cf8b5
Neil <neil@qtum.org>
2019-09-16 01:02:58 +0000

    disable .private_extern

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -4,10 +4,11 @@ $(package)_sha256_hash=87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea
 $(package)_build_subdir=$(package)-$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
 $(package)_download_path=https://gmplib.org/download/$(package)
-$(package)_patches=0001-fix-darwin-gitian-configure.patch
+$(package)_patches=0001-fix-darwin-gitian-configure.patch 0002-disable-private-extern-in-darwin.patch
 
 define $(package)_preprocess_cmds
-   patch -p1 < $($(package)_patch_dir)/0001-fix-darwin-gitian-configure.patch
+   patch -p1 < $($(package)_patch_dir)/0001-fix-darwin-gitian-configure.patch && \
+   patch -p1 < $($(package)_patch_dir)/0002-disable-private-extern-in-darwin.patch
 endef
 
 define $(package)_config_cmds
