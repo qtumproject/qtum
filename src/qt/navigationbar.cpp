71794f77c797c75e0fdecf6328e198fd894b123b
Time Markov <time.markov@qtum.com>
2019-11-12 15:43:02 +0100

    Reduce navigation bar size

--- src/qt/navigationbar.cpp
+++ src/qt/navigationbar.cpp
@@ -11,9 +11,9 @@
 
 namespace NavigationBar_NS
 {
-static const int ToolButtonWidth = 220;
+static const int ToolButtonWidth = 190;
 static const int ToolButtonHeight = 54;
-static const int ToolButtonIconSize = 32;
+static const int ToolButtonIconSize = 28;
 static const int MarginLeft = 0;
 static const int MarginRight = 0;
 static const int MarginTop = 0;
@@ -101,7 +101,7 @@ protected:
 
             // Determine area
             QRect rect = toolbutton->rect;
-            int w = rect.width() - m_subPaddingRight - m_subPaddingLeft;
+            int w = rect.width() - m_subPaddingRight;
             w = qMax(w, 0);
             rect.setWidth(w);
             rect.setLeft(m_subPaddingLeft);
