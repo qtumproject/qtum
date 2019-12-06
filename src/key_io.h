378ac778e0ed7de89e26968fd747dc8c42e04598
timemarkovqtum <time.markov@qtum.com>
2019-12-06 15:38:59 +0100

    Port key

--- src/key_io.h
+++ src/key_io.h
@@ -25,5 +25,10 @@ std::string EncodeDestination(const CTxDestination& dest);
 CTxDestination DecodeDestination(const std::string& str);
 bool IsValidDestinationString(const std::string& str);
 bool IsValidDestinationString(const std::string& str, const CChainParams& params);
+bool IsValidContractSenderAddressString(const std::string& str);
+
+#ifdef ENABLE_BITCORE_RPC
+bool DecodeIndexKey(const std::string& str, uint256& hashBytes, int& type);
+#endif
 
 #endif // BITCOIN_KEY_IO_H
