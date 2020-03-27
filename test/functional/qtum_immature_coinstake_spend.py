e58b201329e3bd4d81603543b142ed4da56bf916
djaen <david.jaenson@gmail.com>
2020-03-27 22:59:07 +0100

    Set offlinestakingheight by default, set lastmposheight by default for mpos tests

--- test/functional/qtum_immature_coinstake_spend.py
+++ test/functional/qtum_immature_coinstake_spend.py
@@ -14,7 +14,7 @@ class QtumPrematureCoinstakeSpendTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
-        self.extra_args = [['-acceptnonstdtxn']]
+        self.extra_args = [['-acceptnonstdtxn', '-lastmposheight=9999999']]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
