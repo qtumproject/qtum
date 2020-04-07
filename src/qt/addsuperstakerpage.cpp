b7220abb19a12b6cb7cfebbb96c2518103d8d4f3
timemarkovqtum <time.markov@qtum.com>
2020-04-07 14:58:52 +0200

    Check is super staking is enabled

--- src/qt/addsuperstakerpage.cpp
+++ src/qt/addsuperstakerpage.cpp
@@ -1,8 +1,10 @@
 #include "addsuperstakerpage.h"
 #include "qt/forms/ui_addsuperstakerpage.h"
 
+#include <miner.h>
 #include <wallet/wallet.h>
 #include <qt/walletmodel.h>
+#include <QMessageBox>
 
 AddSuperStakerPage::AddSuperStakerPage(QWidget *parent) :
     QDialog(parent),
@@ -80,6 +82,12 @@ void AddSuperStakerPage::on_addSuperStakerButton_clicked()
 {
     if(m_model)
     {
+        bool fSuperStake = gArgs.GetBoolArg("-superstaking", DEFAULT_SUPER_STAKE);
+        if(!fSuperStake)
+        {
+            QMessageBox::information(this, tr("Super staking"), tr("Enable super staking from the option menu in order to start the super staker."));
+        }
+
         QString stakerAddress = ui->lineEditStaker->currentText();
         int stakerFee = ui->spinBoxFee->value();
         interfaces::SuperStakerInfo superStaker;
