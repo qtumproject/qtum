aba3b57cd04125639a694d032470de42fd3ad3ff
timemarkovqtum <time.markov@qtum.com>
2020-10-30 13:50:14 +0100

    Update hardware keystore dialog

--- src/qt/hardwarekeystoredialog.cpp
+++ src/qt/hardwarekeystoredialog.cpp
@@ -4,15 +4,53 @@
 
 #include <qt/hardwarekeystoredialog.h>
 #include <qt/forms/ui_hardwarekeystoredialog.h>
+#include <QRadioButton>
+#include <QVBoxLayout>
 
-HardwareKeystoreDialog::HardwareKeystoreDialog(QWidget *parent) :
+class HardwareKeystoreDialogPriv
+{
+public:
+    QList<QRadioButton*> devices;
+};
+
+HardwareKeystoreDialog::HardwareKeystoreDialog(const QStringList& devices, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::HardwareKeystoreDialog)
 {
     ui->setupUi(this);
+
+    d = new HardwareKeystoreDialogPriv();
+    QVBoxLayout* boxLayout = new QVBoxLayout(ui->groupBoxDevices);
+    for(QString deviceName : devices)
+    {
+        QRadioButton *deviceWidget = new QRadioButton(deviceName, this);
+        boxLayout->addWidget(deviceWidget);
+        d->devices.push_back(deviceWidget);
+    }
+    boxLayout->addStretch(1);
+    setCurrentIndex(0);
 }
 
 HardwareKeystoreDialog::~HardwareKeystoreDialog()
 {
     delete ui;
+    delete d;
+}
+
+int HardwareKeystoreDialog::currentIndex() const
+{
+    for(int index = 0; index < d->devices.size(); index++)
+    {
+        if(d->devices[index]->isChecked())
+            return index;
+    }
+    return -1;
+}
+
+void HardwareKeystoreDialog::setCurrentIndex(int index)
+{
+    if(index >=0 && index < d->devices.size())
+    {
+        d->devices[index]->setChecked(true);
+    }
 }
--- src/qt/hardwarekeystoredialog.h
+++ src/qt/hardwarekeystoredialog.h
@@ -2,21 +2,27 @@
 #define HARDWAREKEYSTOREDIALOG_H
 
 #include <QDialog>
+#include <QStringList>
 
 namespace Ui {
 class HardwareKeystoreDialog;
 }
+
+class HardwareKeystoreDialogPriv;
 
 class HardwareKeystoreDialog : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit HardwareKeystoreDialog(QWidget *parent = 0);
+    explicit HardwareKeystoreDialog(const QStringList& devices, QWidget *parent = 0);
     ~HardwareKeystoreDialog();
+    int currentIndex() const;
+    void setCurrentIndex(int index);
 
 private:
     Ui::HardwareKeystoreDialog *ui;
+    HardwareKeystoreDialogPriv *d;
 };
 
 #endif // HARDWAREKEYSTOREDIALOG_H
