cde6a5f09bec8c670905ff2f66859ececb5d4078
timemarkovqtum <time.markov@qtum.com>
2021-09-23 10:23:06 +0200

    Add InvalidCode unit tests

--- src/test/qtumtests/londonfork_tests.cpp
+++ src/test/qtumtests/londonfork_tests.cpp
@@ -28,13 +28,35 @@ const std::vector<valtype> CODE = {
             selfdestruct(addr); 
         }
     }
-    */    valtype(ParseHex("608060405234801561001057600080fd5b50336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550610106806100606000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c806315e812ad14603757806343d726d6146051575b600080fd5b603d6059565b6040516048919060ad565b60405180910390f35b60576061565b005b600048905090565b60008060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1690508073ffffffffffffffffffffffffffffffffffffffff16ff5b60a78160c6565b82525050565b600060208201905060c0600083018460a0565b92915050565b600081905091905056fea2646970667358221220533ac75fbef0edfb29e840b19d4f3c851ec9e4dbf5a00840d68785006f370fe264736f6c63430008070033")),
+    */
+    valtype(ParseHex("608060405234801561001057600080fd5b50336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550610106806100606000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c806315e812ad14603757806343d726d6146051575b600080fd5b603d6059565b6040516048919060ad565b60405180910390f35b60576061565b005b600048905090565b60008060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1690508073ffffffffffffffffffffffffffffffffffffffff16ff5b60a78160c6565b82525050565b600060208201905060c0600083018460a0565b92915050565b600081905091905056fea2646970667358221220533ac75fbef0edfb29e840b19d4f3c851ec9e4dbf5a00840d68785006f370fe264736f6c63430008070033")),
 
     // getBaseFee()
     valtype(ParseHex("15e812ad")),
 
     // close()
-    valtype(ParseHex("43d726d6"))
+    valtype(ParseHex("43d726d6")),
+
+    // deploy one byte ef
+    valtype(ParseHex("60ef60005360016000f3")),
+
+    // deploy two bytes ef00
+    valtype(ParseHex("60ef60005360026000f3")),
+
+    // deploy three bytes ef0000
+    valtype(ParseHex("60ef60005360036000f3")),
+
+    // deploy 32 bytes ef00...00
+    valtype(ParseHex("60ef60005360206000f3")),
+
+    // deploy one byte fe
+    valtype(ParseHex("60fe60005360016000f3")),
+
+    // call with first byte ef
+    valtype(ParseHex("ef653660")),
+
+    // call with second byte ef
+    valtype(ParseHex("60ef6536"))
 };
 
 void genesisLoading(){
@@ -97,6 +119,27 @@ BOOST_AUTO_TEST_CASE(checking_london_after_fork){
         BOOST_CHECK(result.first[0].execRes.gasRefunded == 0);
         BOOST_CHECK(result.first[0].execRes.excepted == dev::eth::TransactionException::None);
     }
+
+    {
+        // Call invalidcode
+        dev::Address proxy = createQtumAddress(txs[0].getHashWith(), txs[0].getNVout());
+        std::vector<QtumTransaction> txIsItLondon;
+        txIsItLondon.push_back(createQtumTransaction(CODE[3], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[4], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[5], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[6], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[7], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[8], 0, GASLIMIT, dev::u256(1), ++hashTx, proxy));
+        txIsItLondon.push_back(createQtumTransaction(CODE[9], 0, GASLIMIT, dev::u256(1), ++hashTx, proxy));
+        auto result = executeBC(txIsItLondon);
+        BOOST_CHECK(result.first[0].execRes.excepted == dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[1].execRes.excepted == dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[2].execRes.excepted == dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[3].execRes.excepted == dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[4].execRes.excepted != dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[5].execRes.excepted != dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[6].execRes.excepted != dev::eth::TransactionException::InvalidCode);
+    }
 }
 
 BOOST_AUTO_TEST_CASE(checking_london_before_fork){
@@ -127,6 +170,27 @@ BOOST_AUTO_TEST_CASE(checking_london_before_fork){
         auto result = executeBC(txIsItLondon);
         BOOST_CHECK(result.first[0].execRes.gasRefunded == 24000);
         BOOST_CHECK(result.first[0].execRes.excepted == dev::eth::TransactionException::None);
+    }
+
+    {
+        // Call invalidcode
+        dev::Address proxy = createQtumAddress(txs[0].getHashWith(), txs[0].getNVout());
+        std::vector<QtumTransaction> txIsItLondon;
+        txIsItLondon.push_back(createQtumTransaction(CODE[3], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[4], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[5], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[6], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[7], 0, GASLIMIT, dev::u256(1), ++hashTx, dev::Address()));
+        txIsItLondon.push_back(createQtumTransaction(CODE[8], 0, GASLIMIT, dev::u256(1), ++hashTx, proxy));
+        txIsItLondon.push_back(createQtumTransaction(CODE[9], 0, GASLIMIT, dev::u256(1), ++hashTx, proxy));
+        auto result = executeBC(txIsItLondon);
+        BOOST_CHECK(result.first[0].execRes.excepted != dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[1].execRes.excepted != dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[2].execRes.excepted != dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[3].execRes.excepted != dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[4].execRes.excepted != dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[5].execRes.excepted != dev::eth::TransactionException::InvalidCode);
+        BOOST_CHECK(result.first[6].execRes.excepted != dev::eth::TransactionException::InvalidCode);
     }
 }
 
