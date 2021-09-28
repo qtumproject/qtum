abaa1421f2254907ee8af91f0dfd212447289e9b
timemarkovqtum <time.markov@qtum.com>
2021-09-28 15:03:25 +0200

    Update precompiled utils to print error messages

--- src/test/qtumtests/precompiled_utils.h
+++ src/test/qtumtests/precompiled_utils.h
@@ -21,7 +21,8 @@ public:
                       const dev::eth::ChainOperationParams& _chainParams,
                       const dev::u256& _blockNumber):
         chainParams(_chainParams),
-        blockNumber(_blockNumber)
+        blockNumber(_blockNumber),
+        callName(_name)
     {
         // Get the executor and gas pricer for the precompiled contract
         exec = dev::eth::PrecompiledRegistrar::executor(_name);
@@ -38,8 +39,8 @@ public:
         UniValue json_tests = read_json(jsondata);
 
         // Check the executor and gas pricer
-        BOOST_CHECK(exec);
-        BOOST_CHECK(cost);
+        BOOST_CHECK_MESSAGE(exec, strprintf("Executor not found for precompiled contract %s", callName));
+        BOOST_CHECK_MESSAGE(cost, strprintf("Pricer not found for precompiled contract %s", callName));
 
         if(exec && cost)
         {
@@ -50,30 +51,33 @@ public:
                 const UniValue& tv = json_tests[idx];
                 std::string strInput = tv["Input"].get_str();
                 std::string strExpected = tv["Expected"].get_str();
+                std::string strName = tv["Name"].get_str();
                 int gas = tv["Gas"].get_int();
 
                 dev::bytes in = dev::fromHex(strInput);
                 dev::bytes expected = dev::fromHex(strExpected);
 
                 // Check the precompiled contract
-                pricerTest(in, gas);
-                executorTest(in, expected);
+                pricerTest(in, gas, strName);
+                executorTest(in, expected, strName);
             }
         }
     }
 
 private:
-    void pricerTest(const dev::bytes& in, const int& gas)
+    void pricerTest(const dev::bytes& in, const int& gas, const std::string& testName)
     {
         auto res = cost(dev::bytesConstRef(in.data(), in.size()), chainParams, blockNumber);
         BOOST_CHECK_EQUAL(static_cast<int>(res), gas);
+        BOOST_CHECK_MESSAGE(static_cast<int>(res) == gas, strprintf("Gas not correct for precompiled contract %s in test %s", callName, testName));
     }
 
-    void executorTest(const dev::bytes& in, const dev::bytes& expected)
+    void executorTest(const dev::bytes& in, const dev::bytes& expected, const std::string& testName)
     {
         auto res = exec(dev::bytesConstRef(in.data(), in.size()));
         BOOST_CHECK(res.first);
         BOOST_CHECK_EQUAL_COLLECTIONS(res.second.begin(), res.second.end(), expected.begin(), expected.end());
+        BOOST_CHECK_MESSAGE(res.second == expected, strprintf("Output not correct for precompiled contract %s in test %s", callName, testName));
     }
 
     UniValue read_json(const std::string& jsondata)
@@ -92,6 +96,7 @@ private:
     dev::eth::PrecompiledPricer cost;
     dev::eth::ChainOperationParams chainParams;
     dev::u256 blockNumber;
+    std::string callName;
 };
 
 #define RunPrecompiledTests(contract, data, params, blockNumber)\
