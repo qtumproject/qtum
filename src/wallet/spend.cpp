d541fae99f30104a882f1fc5fc1e556eadbe3b1a
timemarkovqtum <time.markov@qtum.com>
2022-02-09 13:55:27 +0100

    Fix signing transaction outputs

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -872,34 +872,10 @@ bool CWallet::CreateTransactionInternal(
         return false;
     }
 
-    if (sign)
-    {
-        // Signing transaction outputs
-        int nOut = 0;
-        for (const auto& output : txNew.vout)
-        {
-            if(output.scriptPubKey.HasOpSender())
-            {
-                const CScript& scriptPubKey = GetScriptForDestination(signSenderAddress);
-                SignatureData sigdata;
-
-                auto spk_man = GetLegacyScriptPubKeyMan();
-                if (!ProduceSignature(*spk_man, MutableTransactionSignatureOutputCreator(&txNew, nOut, output.nValue, SIGHASH_ALL), scriptPubKey, sigdata))
-                {
-                    error = _("Signing transaction output failed");
-                    return false;
-                }
-                else
-                {
-                    if(!UpdateOutput(txNew.vout.at(nOut), sigdata))
-                    {
-                        error = _("Update transaction output failed");
-                        return false;
-                    }
-                }
-            }
-            nOut++;
-        }
+    // Signing transaction outputs
+    if(sign && txNew.HasOpSender() && !SignTransactionOutput(txNew)) {
+        error = _("Signing transaction output failed");
+        return false;
     }
 
     if (sign && !SignTransaction(txNew)) {
