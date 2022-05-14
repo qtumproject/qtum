a9d37df9a4e656272958ac4b3fc99fe0e4098441
Neil <neil@qtum.org>
2022-05-14 06:54:45 +0100

    Update EVM globals test

--- test/functional/qtum_globals_state_changer.py
+++ test/functional/qtum_globals_state_changer.py
@@ -19,44 +19,55 @@ class QtumGlobalsStateChangerTest(BitcoinTestFramework):
         self.nodes[0].generate(10 + COINBASE_MATURITY)
 
         """
-        pragma solidity ^0.4.12;
-
+        pragma solidity ^0.8.0;
+        
         contract Test {
             uint public stateChanger;
             
-            function blkblockhash() payable {
-                stateChanger = uint(block.blockhash(block.number-1));
+            function blkblockhash() public payable {
+                stateChanger = uint(blockhash(block.number-1));
             }
-
-            function blkcoinbase() payable {
-                stateChanger = uint(block.coinbase);
+        
+            function blkcoinbase() public payable {
+                stateChanger = uint(uint160(address(block.coinbase)));
             }
-
-            function blkdifficulty() payable {
+        
+            function blkdifficulty() public payable {
                 stateChanger = block.difficulty;
             }
-
-            function blkgaslimit() payable {
+        
+            function blkgaslimit() public payable {
                 stateChanger = block.gaslimit;
             }
-
-            function blknumber() payable {
+        
+            function blknumber() public payable {
                 stateChanger = block.number;
             }
             
-            function blktimestamp() payable {
+            function blktimestamp() public payable {
                 stateChanger = block.timestamp;
             }
+        
+            function blkbasefee() public payable {
+                stateChanger = block.basefee;
+            }
+        
+            function blkchainid() public payable {
+                stateChanger = block.chainid;
+            }
         }
-        73c9330d blkblockhash()
-        5facaf8e blkcoinbase()
-        009e8657 blkdifficulty()
-        49b573d4 blkgaslimit()
-        6bde2d73 blknumber()
-        c1a5257b blktimestamp()
-        6c7804b5 stateChanger()
+        Function signatures:
+        2436c165: blkbasefee()
+        73c9330d: blkblockhash()
+        12a7e88c: blkchainid()
+        5facaf8e: blkcoinbase()
+        009e8657: blkdifficulty()
+        49b573d4: blkgaslimit()
+        6bde2d73: blknumber()
+        c1a5257b: blktimestamp()
+        6c7804b5: stateChanger()
         """
-        contract_bytecode = "6060604052341561000f57600080fd5b5b6101738061001f6000396000f30060606040523615610080576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680629e86571461008557806349b573d41461008f5780635facaf8e146100995780636bde2d73146100a35780636c7804b5146100ad57806373c9330d146100d6578063c1a5257b146100e0575b600080fd5b61008d6100ea565b005b6100976100f4565b005b6100a16100fe565b005b6100ab61011e565b005b34156100b857600080fd5b6100c0610128565b6040518082815260200191505060405180910390f35b6100de61012e565b005b6100e861013d565b005b446000819055505b565b456000819055505b565b4173ffffffffffffffffffffffffffffffffffffffff166000819055505b565b436000819055505b565b60005481565b4340600190046000819055505b565b426000819055505b5600a165627a7a72305820a4033d29ccf12dc58fda987b64dec57272d205023628eb3be8ba613585453d770029"
+        contract_bytecode = "608060405234801561001057600080fd5b50610246806100206000396000f3fe6080604052600436106100855760003560e01c80635facaf8e116100595780635facaf8e146100b25780636bde2d73146100bc5780636c7804b5146100c657806373c9330d146100f1578063c1a5257b146100fb57610085565b80629e86571461008a57806312a7e88c146100945780632436c1651461009e57806349b573d4146100a8575b600080fd5b610092610105565b005b61009c61010e565b005b6100a6610117565b005b6100b0610120565b005b6100ba610129565b005b6100c4610148565b005b3480156100d257600080fd5b506100db610151565b6040516100e89190610192565b60405180910390f35b6100f9610157565b005b610103610170565b005b44600081905550565b46600081905550565b48600081905550565b45600081905550565b4173ffffffffffffffffffffffffffffffffffffffff16600081905550565b43600081905550565b60005481565b60014361016491906101dc565b4060001c600081905550565b42600081905550565b6000819050919050565b61018c81610179565b82525050565b60006020820190506101a76000830184610183565b92915050565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052601160045260246000fd5b60006101e782610179565b91506101f283610179565b925082821015610205576102046101ad565b5b82820390509291505056fea264697066735822122050fc4f59959598b20e40c132b87c4c613b711382aeb5aa25db716706ba7010b264736f6c634300080d0033"
         contract_address = self.nodes[0].createcontract(contract_bytecode)['address']
         self.node.generate(1)
 
@@ -93,6 +104,18 @@ class QtumGlobalsStateChangerTest(BitcoinTestFramework):
         print("State change based on block.timestamp")
         block_count = self.node.getblockcount()
         self.node.sendtocontract(contract_address, "c1a5257b", 0, 1000000)
+        self.node.generate(1)
+        assert_equal(self.node.getblockcount(), block_count+1)
+        
+        print("State change based on block.basefee")
+        block_count = self.node.getblockcount()
+        self.node.sendtocontract(contract_address, "2436c165", 0, 1000000)
+        self.node.generate(1)
+        assert_equal(self.node.getblockcount(), block_count+1)
+        
+        print("State change based on block.chainid")
+        block_count = self.node.getblockcount()
+        self.node.sendtocontract(contract_address, "12a7e88c", 0, 1000000)
         self.node.generate(1)
         assert_equal(self.node.getblockcount(), block_count+1)
 
