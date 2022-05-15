0beaa57abfb224af01de66d21ad1e8bea6316b1c
Neil <neil@qtum.org>
2022-05-15 03:56:58 +0100

    WIP london activation test

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -309,7 +309,8 @@ BASE_SCRIPTS = [
     # Put them in a random line within the section that fits their approximate run-time
 	
 	# qtum
-    'qtum_evm_london.py',
+    'qtum_evm_london_activation.py',
+    'qtum_evm_london_gas_usage.py',
     'qtum_dgp.py',
     'qtum_pos.py',
     'qtum_opcall.py',
