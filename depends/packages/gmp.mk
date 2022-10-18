3d6d82bdb3b61306ac976baa908f008365ab1b76
timemarkovqtum <time.markov@qtum.com>
2022-10-18 15:51:20 +0200

    Update packages

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,11 +1,12 @@
 package=gmp
-$(package)_version=6.1.2
-$(package)_sha256_hash=87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea0e21912
+$(package)_version=6.2.1
+$(package)_sha256_hash=fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
 $(package)_download_path=https://gmplib.org/download/$(package)
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --enable-cxx
+  $(package)_config_opts=--prefix=$(host_prefix)
+  $(package)_config_opts += --disable-shared --enable-static --enable-cxx
   $(package)_config_opts += --enable-option-checking
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_darwin += --with-pic
@@ -25,4 +26,8 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  sed -i "s|libstdc++.la|libstdc++.a|" lib/libgmpxx.la
 endef
