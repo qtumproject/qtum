618673328fb877e0f4cbf3d891028b21c3e671bd
meherett <meherett.batu@gmail.com>
2024-06-10 09:28:35 +0300

    Add: fee_rate parm on create_self_transfer_chain

--- test/functional/test_framework/wallet.py
+++ test/functional/test_framework/wallet.py
@@ -378,7 +378,7 @@ class MiniWallet:
         self.scan_tx(from_node.decoderawtransaction(tx_hex))
         return txid
 
-    def create_self_transfer_chain(self, *, chain_length, utxo_to_spend=None):
+    def create_self_transfer_chain(self, *, chain_length, utxo_to_spend=None, fee_rate=Decimal("0.003")):
         """
         Create a "chain" of chain_length transactions. The nth transaction in
         the chain is a child of the n-1th transaction and parent of the n+1th transaction.
@@ -387,7 +387,7 @@ class MiniWallet:
         chain = []
 
         for _ in range(chain_length):
-            tx = self.create_self_transfer(utxo_to_spend=chaintip_utxo)
+            tx = self.create_self_transfer(utxo_to_spend=chaintip_utxo, fee_rate=fee_rate)
             chaintip_utxo = tx["new_utxo"]
             chain.append(tx)
 
