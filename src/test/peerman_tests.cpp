7466a0f0c5705eb5d057405ad6f13201ac61c5ee
timemarkovqtum <time.markov@qtum.com>
2024-08-30 14:51:27 +0200

    Fix unit test peerman_tests

--- src/test/peerman_tests.cpp
+++ src/test/peerman_tests.cpp
@@ -21,6 +21,7 @@ static void mineBlock(const node::NodeContext& node, std::chrono::seconds block_
     auto curr_time = GetTime<std::chrono::seconds>();
     SetMockTime(block_time); // update time so the block is created with it
     CBlock block = node::BlockAssembler{node.chainman->ActiveChainstate(), nullptr}.CreateNewBlock(CScript() << OP_TRUE)->block;
+    node::RegenerateCommitments(block, *Assert(node.chainman)); // Update block.hashMerkleRoot
     while (!CheckProofOfWork(block.GetHash(), block.nBits, node.chainman->GetConsensus())) ++block.nNonce;
     block.fChecked = true; // little speedup
     SetMockTime(curr_time); // process block at current time
