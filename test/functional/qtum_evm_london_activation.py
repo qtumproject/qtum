de7e3ab8d4f2d18d901bfe70e707a5ca33d633a1
Neil <neil@qtum.org>
2024-11-01 01:55:20 +0400

    Add cancun tests

--- test/functional/qtum_evm_london_activation.py
+++ test/functional/qtum_evm_london_activation.py
@@ -15,6 +15,9 @@ from decimal import Decimal
 pp = pprint.PrettyPrinter()
 
 class QtumEVMLondonTest(BitcoinTestFramework):
+    def add_options(self, parser):
+        self.add_wallet_options(parser, descriptors=False)
+        
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 4
