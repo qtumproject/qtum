4814afb12b11c33c5446964894ee8440d41e52a1
timemarkovqtum <time.markov@qtum.com>
2025-01-30 11:37:56 +0100

    Fix bench index block filter

--- src/bench/index_blockfilter.cpp
+++ src/bench/index_blockfilter.cpp
@@ -17,11 +17,11 @@ static void BlockFilterIndexSync(benchmark::Bench& bench)
     const auto test_setup = MakeNoLogFileContext<TestChain100Setup>();
 
     // Create more blocks
-    int CHAIN_SIZE = 600;
+    int CHAIN_SIZE = 2500;
     CPubKey pubkey{ParseHex("02ed26169896db86ced4cbb7b3ecef9859b5952825adbeab998fb5b307e54949c9")};
     CScript script = GetScriptForDestination(WitnessV0KeyHash(pubkey));
     std::vector<CMutableTransaction> noTxns;
-    for (int i = 0; i < CHAIN_SIZE - 100; i++) {
+    for (int i = 0; i < CHAIN_SIZE - 2000; i++) {
         test_setup->CreateAndProcessBlock(noTxns, script);
         SetMockTime(GetTime() + 1);
     }
