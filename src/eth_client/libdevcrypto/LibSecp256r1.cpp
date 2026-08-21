13fd733156572e86492930259b04a520a984303c
timemarkovqtum <time.markov@qtum.com>
2026-08-21 13:44:08 +0200

    Fix p256verify precompile

--- src/eth_client/libdevcrypto/LibSecp256r1.cpp
+++ src/eth_client/libdevcrypto/LibSecp256r1.cpp
@@ -9,12 +9,16 @@ using namespace dev::crypto;
 
 pair<bool, bytes> dev::crypto::p256verify(dev::bytesConstRef input)
 {
+    // EIP-7951:
+    // The precompile MUST NOT revert under any circumstances.
+    // Invalid inputs or verification failures MUST return `` and consume the same amount of gas had verification succeeded.
+
     bytes output(32, 0);
     size_t output_size = output.size();
     size_t input_size = input.size();
     assert(output_size >= 32);
     if (input_size != 160)
-        return {false, bytes{}};
+        return {true, bytes{}};
 
     ethash::hash256 h;
     std::memcpy(h.bytes, reinterpret_cast<const uint8_t*>(&input[0]), 32);
@@ -24,7 +28,7 @@ pair<bool, bytes> dev::crypto::p256verify(dev::bytesConstRef input)
     intx::uint256 qy = intx::be::unsafe::load<intx::uint256>(reinterpret_cast<const uint8_t*>(&input[128]));
 
     if (!evmmax::secp256r1::verify(h, r, s, qx, qy))
-        return {false, bytes{}};
+        return {true, bytes{}};
 
     output[31] = 1;
     return {true, output};
